dad5b89e63dde55bf48ac27f84f6b3e8a3288005
Rutuja341 <rutujak341@gmail.com>
2023-12-14 14:43:22 +0530

    Added coupons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { SortComponent } from './sort/sort.component';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { FilterComponent } from './filter/filter.component';
+import { CouponsComponent } from './coupons/coupons.component';
+import { MatTableModule} from '@angular/material/table';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,6 +33,7 @@ import { FilterComponent } from './filter/filter.component';
     RegistrationComponent,
     SortComponent,
     FilterComponent,
+    CouponsComponent,
   ],
   imports: [
     BrowserModule,
@@ -50,6 +53,7 @@ import { FilterComponent } from './filter/filter.component';
     ToastrModule.forRoot(),
     MatRadioModule,
     MatDialogModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent],
