18c6150dc356f80d8f16bd42cb159cf7d009ee29
PriyaDhepe <2016btt501@sggs.ac.in>
2023-12-14 12:02:23 +0530

    discounted car price

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,13 +5,18 @@ import { ToastrService } from 'ngx-toastr';
 import { SortComponent } from '../sort/sort.component';
 import { MatDialog } from '@angular/material/dialog';
 import { FilterComponent } from '../filter/filter.component';
+import { VariableConstants } from '../variable.constant';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  discountPrice: any;
+  discountOnMaruti = 10;
+  // discountOnkiya = VariableConstants.KIYA_DISCOUNT;
   search: any;
+  dicountPrice: any;
   carsFilterData: any = [];
   allcar: any = [];
   cararray: any = [];
@@ -19,14 +24,15 @@ export class DashboardComponent {
     private service: CarsService,
     private sort: MatDialog,
     private dialog: MatDialog,
-    private router : Router,
-    private toastr : ToastrService
+    private router: Router,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit() {
     this.service.getCarInfo().subscribe(
       (res: any) => {
         this.allcar = res.data;
+        this.allCarDiscountPrice();
         this.carsFilterData = this.allcar;
         console.log('dashboardcarsucessed', res);
       },
@@ -35,7 +41,35 @@ export class DashboardComponent {
       }
     );
   }
-
+  allCarDiscountPrice() {
+    this.allcar.forEach((item: any) => {
+      if (item.brand == VariableConstants.MARUTI_SUZUKI) {
+        item.discountPrice = item.price - item.price * 0.1;
+      } else if (item.brand == VariableConstants.Toyota) {
+        item.discountPrice = item.price - item.price * 0.15;
+      } else if (item.brand == VariableConstants.Kia) {
+        item.discountPrice = item.price - item.price * 0.12;
+      } else if (item.brand == VariableConstants.Hyundai) {
+        item.discountPrice = item.price - item.price * 0.13;
+      } else if (item.brand == VariableConstants.Honda) {
+        item.discountPrice = item.price - item.price * 0.16;
+      } else if (item.brand == VariableConstants.Tata) {
+        item.discountPrice = item.price - item.price * 0.15;
+      } else if (item.brand == VariableConstants.Mahindra) {
+        item.discountPrice = item.price - item.price * 0.12;
+      } else if (item.brand == VariableConstants.Ford) {
+        item.discountPrice = item.price - item.price * 0.1;
+      } else if (item.brand == VariableConstants.Renault) {
+        item.discountPrice = item.price - item.price * 0.14;
+      } else if (item.brand == VariableConstants.Volkswagen) {
+        item.discountPrice = item.price - item.price * 0.12;
+      } else if (item.brand == VariableConstants.Chevrolet) {
+        item.discountPrice = item.price - item.price * 0.17;
+      } else if (item.brand == VariableConstants.Fiat) {
+        item.discountPrice = item.price - item.price * 0.12;
+      }
+    });
+  }
   getSortData() {
     const dialogRef = this.sort.open(SortComponent, {
       width: '50',
@@ -49,7 +83,7 @@ export class DashboardComponent {
   }
   getFilterData() {
     const dialogRef = this.dialog.open(FilterComponent, {
-      width: '73%',
+      width: '74%',
       height: '45%',
       panelClass: 'my-class',
       data: { name: 'Data' },
@@ -147,10 +181,10 @@ export class DashboardComponent {
   }
 
   Reset() {}
-  logOut(){
+  logOut() {
     localStorage.removeItem('token');
     localStorage.removeItem('userType');
     this.router.navigateByUrl('');
-    this.toastr.success('Logged out.')
+    this.toastr.success('Logged out.');
   }
 }
